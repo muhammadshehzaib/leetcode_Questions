7b6ac706a7aefd5f0b739674ac37300790b0af28
Muhammad Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2024-08-14 04:23:57 -0700

    Time: 7 ms (48.29%), Space: 15.1 MB (65.87%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.cpp
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        vector<int> nums1;
-        for(int i=0;i<nums.size();i++){
-            nums1.push_back(nums[i]);
+        int n = nums.size();
+        vector<int> result(2*n);
+        for(int i=0;i<n;i++){
+            result[i] = nums[i];
+            result[i+n] = nums[i];
         }
-        for(int i=0;i<nums.size();i++){
-            nums1.push_back(nums[i]);
-        }
-        return nums1;
+
+        return result;
     }
 };
