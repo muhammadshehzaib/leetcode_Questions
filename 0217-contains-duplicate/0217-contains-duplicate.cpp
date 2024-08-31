593942cf1ac58e6a304149da6ff512b36b208b55
Muhammad Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2024-08-31 11:30:55 -0700

    Time: 136 ms (9%), Space: 76.1 MB (7.32%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
-                return true;
-            }
+        std::set<int> sets(nums.begin(), nums.end());
+        if(nums.size()>sets.size()){
+            return true;
         }
         return false;
+
     }
 };
