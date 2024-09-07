83953de5d74920eb8078e7444480857d375b5f41
Muhammad Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2024-09-07 02:57:54 -0700

    Time: 10 ms (8.95%), Space: 9.1 MB (46.05%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+        string result;
+        if(s.size()==1){
+            return true;
+        }
+        for(char ch:s){
+           if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')) {
+                result += ch;
+            }   
+        }
+        cout<<result;
+        int i=0;
+        int j=result.size()-1;
+        while(i<j){
+            if(result[i]!=result[j]){
+                return false;
+            }
+            i++;j--;
+        }
+        return true;
+
+    }
+};
