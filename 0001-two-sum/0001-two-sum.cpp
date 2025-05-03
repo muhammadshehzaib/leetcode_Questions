70d292d0e5c170b89c50bd1b6f47c644eb682bda
Muhammad Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2025-05-03 10:37:35 +0500

    Time: 147 ms (5.08%), Space: 14.2 MB (58.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> vec;
-        for(int i=0;i<nums.size()-1;i++){
-            for(int j=i+1;j<nums.size();j++){         
-
-            if(nums[i]+nums[j]==target){
-                vec.push_back(i);
-                vec.push_back(j);
-                break;
+        vector<int> a;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    a.push_back(i);
+                    a.push_back(j);
+                }
             }
         }
-    }
-        return vec;
+        return a;
     }
 };
